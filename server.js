bbc5605f6bc1a645f7a6e23d69fb474f6cedfd40
GStar13 <ginazavala004@gmail.com>
2019-03-28 20:35:42 -0700

    anytype datavalue=true

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//server.js
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
@@ -8,7 +9,7 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
